de19b63f2982aa4f0db977d1fed5783c8b01ecbb
yashAnjaliGupta <yashgupta.yg318@gmail.com>
2025-05-02 09:15:29 +0530

    Initial commit

--- src/components/ASTdispalybox.tsx
+++ src/components/ASTdispalybox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {codeToAst,sqlAstToGraph,getAllTableNodesAsTableNodes} from '../parser/parser';
+import {codeToAst,sqlAstToGraph,getAllTableNodesAsTableNodes} from '../parser/graphGenerator';
 
 interface ASTDisplayBoxProps {
     input: string;
@@ -12,9 +12,10 @@ const ASTDisplayBox: React.FC<ASTDisplayBoxProps> = ({ input }) => {
     // // console.log(JSON.stringify(g, null, 2));
     // const temp=getAllTableNodesAsTableNodes(g);
     // console.log(temp);
+    const astString = JSON.stringify(ast, null, 2);
     return (
         <div style={{ width: '25vw', height: '70vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', border: '1px solid black' }}>
-            <pre style={{ width: '100%', whiteSpace: 'pre', overflow: 'auto' }}>{ast}</pre>
+            <pre style={{ width: '100%', whiteSpace: 'pre', overflow: 'auto' }}>{astString}</pre>
         </div>
     );
 };
